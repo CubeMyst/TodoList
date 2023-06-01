59c747d19c4dc25e87bdba4e055e089bc2bb9d3d
YeiserDeveloper <yeiser192@gmail.com>
2023-05-31 23:46:12 -0400

    Changing the settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {
-            fontFamily: {
-                anton: "'Anton', sans-serif",
-                inter: "'Inter', sans-serif",
-            }
-        },
-    },
-    plugins: [],
-}
+	content: ['/index.html', './src/**/*.{js,ts,jsx,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				anton: "'Anton', sans-serif",
+				inter: "'Inter', sans-serif",
+			},
+		},
+	},
+	plugins: [require("daisyui")],
+};
